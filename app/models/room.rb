a99344501fecb02ac1935f1b1bdc5ea8eaddd1bb
Amanda Peterson <amandaleighp@gmail.com>
2015-03-03 06:39:52 -0800

    Added bootstrap foundation

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,6 @@
 class Room < ActiveRecord::Base
-has_attached_file :preview, :styles => { :medium => "500x500>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :preview, :content_type => /\Aimage\/.*\Z/
-  validates_attachment :preview, size: { in: 0..120.megabytes}
+	has_attached_file :preview, :styles => { :medium => "500x500>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	  validates_attachment_content_type :preview, :content_type => /\Aimage\/.*\Z/
+	  validates_attachment :preview, size: { in: 0..120.megabytes}
+	belongs_to :user
 end
